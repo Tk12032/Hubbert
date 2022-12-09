6713bd00dfb9925beb4d8c9564228893c22ccacf
LokumBek <lukmanzel007@gmail.com>
2022-12-09 15:13:07 +0100

    Update header.php

--- header.php
+++ header.php
@@ -17,7 +17,7 @@
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+      <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
         <li class="nav-item">
           <a class="nav-link" href="#"><button>Demander un service / outil</button></a>
         </li>
