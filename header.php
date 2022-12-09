987b3fb54cb6d7a96373fa4eecfb7cacb897c930
LokumBek <lukmanzel007@gmail.com>
2022-12-09 15:05:29 +0100

    reparations du css

--- header.php
+++ header.php
@@ -16,8 +16,8 @@
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
-    <div class="collapse navbar-collapse droite" id="navbarTogglerDemo02">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0 droite">
+    <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
         <li class="nav-item">
           <a class="nav-link" href="#"><button>Demander un service / outil</button></a>
         </li>
