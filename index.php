00e715dff4d12d62dfae88830071315809c144da
Tk1203 <leo.mangez@gmail.com>
2022-12-09 12:59:59 +0100

    début de structrure html page acceuil

--- index.php
+++ index.php
@@ -1,4 +1,39 @@
 <?php get_header(); ?>
 
+<button class="messagerie"><!-- boutton messagerie --></button>    
+
+<section class="garden">
+        <div>
+            <span>
+                <h1></h1>
+                <p></p>
+            </span>
+            <button></button>
+        </div>
+    </section>
+
+    <section class="services">
+        <div class="carroussel users">
+            <!-- fct php de création de card en ftc des meilleurs users -->
+        </div>
+        <div>
+            <button></button>
+            <button></button>
+        </div>
+    </section>
+
+    <section class="outils">
+        <span>
+            <h2></h2>
+            <p></p>
+        </span>
+        <div class="carroussel tools">
+            <!-- fct php de création de card en ftc des meilleurs annonces d'outils -->
+        </div>
+        <div>
+            <button></button>
+            <button></button>
+        </div>
+    </section>
 
 <?php get_footer(); ?>
