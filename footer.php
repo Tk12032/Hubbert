c551f152ab81131ea04d876a05cff093b1244601
Tk1203 <leo.mangez@gmail.com>
2022-12-10 11:12:13 +0100

    footer + fond + ombre header

--- footer.php
+++ footer.php
@@ -1,4 +1,48 @@
+<footer>
+    <div>
+        <span class="border-bot">
+            <h5>SUIVEZ NOUS</h5>
+        <!--ligne-->
+        
+            <a>HubbertBook</a>
+            <a>HubbertGram</a>
+            <a>HubbertTok</a>
+            <a>S'abonner à la newsletter</a>
+        </span>
+        <!--ligne-->
+        <img class="logo-footer" src="<?php echo get_template_directory_uri(); ?>/assets/img/hubbert_noir.png" alt="logo_Hubbert">
+    </div>
+    <div class="text-center">
+        <span>
+            <h5>CONTACTEZ NOUS</h5>
+            <!--ligne-->
+            <p>hubbert.support@gmail.com</p>
+        </span>
+        
+        <span>
+            <h5>MENTIONS LEGALES</h5>
+        <!--ligne-->
+            <a>Conditions générales d'utilisation</a>
+            <a>Conditions générales de ventes</a>
+        </span>
+        <span>
+            <h5>BESOIN D'AIDES ?</h5>
+            <!--ligne-->
+            <a>FAQ</a>
+        </span>
+        
+    </div>
+    <div>
+        <span>
+            <h5>A PROPOS DE NOUS</h5>
+            <!--ligne-->
+            <p class="text-justify">Hubbert est une initiative étudiante qui a la volonté de diminuer l’emprunte carbone de chacun. C’est pourquoi nous vous proposons une plateforme qui vous permet de louer des outils de jardinage et rendre des services aux personnes qui en ont besoin dans leur jardin. </p>
+        </span>
+        
+    </div>
 
+
+</footer>
 </body>
 </html>
 
