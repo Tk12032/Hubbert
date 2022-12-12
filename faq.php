7767ba4376ba68d6649f4a2118c2e47eb5bac623
LokumBek <lukmanzel007@gmail.com>
2022-12-12 12:47:48 +0100

    lol

--- faq.php
+++ faq.php
@@ -1,13 +1,4 @@
-<!DOCTYPE html>
-<html lang="fr">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>FAQ</title>
-    <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/assets/css/app.css">
-</head>
-<body>
+<?php get_header(); ?>
 
 <section class="faq">
         <div>
@@ -16,8 +7,7 @@
                 <p>Alors de ce que nous savons, il est très déconseillé de nourrir ses animaux de compagnie avec du chocolat. Pour la pluspart c’est un aliment qui peut agir comme du poison pour eux. Malgré tout, nous restons assez confu sur la pertinence de cette question sur un site parlant de jardinage.</p>
             </span>
         </div>
-    </section>
+ </section>
 
-</body>
-</html>
 
+<?php get_footer(); ?>
