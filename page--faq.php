af76ec11eb3f8933f7e7febdcb337e3b0bfc0882
LokumBek <lukmanzel007@gmail.com>
2022-12-12 15:42:15 +0100

    lolol

--- page--faq.php
+++ page--faq.php
@@ -2,14 +2,16 @@
 /* Template Name: PageFAQ */
 get_header(); ?>
 
+<div class="container">
 <section class="faq">
-        <div>
+        <div id="q1">
             <span>
                 <h1>Peut on donner du chocolat à son chien ?</h1>
                 <p>Alors de ce que nous savons, il est très déconseillé de nourrir ses animaux de compagnie avec du chocolat. Pour la pluspart c’est un aliment qui peut agir comme du poison pour eux. Malgré tout, nous restons assez confu sur la pertinence de cette question sur un site parlant de jardinage.</p>
             </span>
         </div>
  </section>
+</div>
 
 
 <?php get_footer(); ?>
