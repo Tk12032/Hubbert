006b75c834e892ebae98afd81c1210aa80500b9c
Tk1203 <leo.mangez@gmail.com>
2022-12-13 14:38:49 +0100

    menu header presque enfin fini

--- header.php
+++ header.php
@@ -23,7 +23,7 @@
             wp_nav_menu([
             'theme_location' => 'navbar',
             'container' => false,
-            'menu_class' => 'navbar-nav me-auto'
+            'menu_class' => 'navbar-nav ml-auto'
             ]); ?>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
