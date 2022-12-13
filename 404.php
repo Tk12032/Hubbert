4c780bbae8ae16f509c1425f066633bcabd4a7e1
Tk1203 <leo.mangez@gmail.com>
2022-12-13 12:22:05 +0100

    truc

--- 404.php
+++ 404.php
@@ -1,7 +1,11 @@
 <?php get_header() ?>
+<div class="container">
 
-<section style="height:1000px">
+<section style="height:1000px" class="404">
     <h1 style="font-size:30px;">Aïe ! Il n'y a rien à voir ici petit chenapan ! Reviens sur la page d'accueil en cliquant sur Hubbert en haut à gauche !</h1>
 </section>
+
+</div>
+
 
 <?php get_footer() ?>
