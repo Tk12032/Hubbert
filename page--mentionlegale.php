11554eb7ddf06d2628b687566355c7f484be1f7a
LokumBek <lukmanzel007@gmail.com>
2022-12-13 12:20:43 +0100

    Update page--mentionlegale.php

--- page--mentionlegale.php
+++ page--mentionlegale.php
@@ -2,6 +2,11 @@
 /* Template Name: PageMention */
 get_header(); ?>
 
+<?php $post_id =40; $queried_post = get_post($post_id); $content = $queried_post->post_content; ?>
+
+<?php echo $content; ?>
+
+<?php $post_id =49; $queried_post = get_post($post_id); $content = $queried_post->post_content; $content = preg_replace("/<img[^>]+\>/i", "test lol", $content); ?>
 
 
 <?php get_footer(); ?>
