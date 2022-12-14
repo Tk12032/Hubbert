c083e4eb4f5cc3853274296848296986128bfc35
Tk1203 <leo.mangez@gmail.com>
2022-12-14 15:43:28 +0100

    dynamisme partout

--- home.php
+++ home.php
@@ -8,14 +8,31 @@ get_header(); ?>
 <section class="garden">
         <div class="texte-central">
             <span>
+                <h1>
                 <?php 
-                    $homepage=get_page_by_title('home-page');
-                    echo var_dump($homepage);
-
-
-                
+                    $homepage = new WP_Query([ // je crée une variable $q
+                        'post_type' => 'page', // la je précise quel post_type je veux (dans mon cas "q")    
+                      ]);
+                      while($homepage->have_posts()){
+                        $homepage->the_post();
+                        echo get_field('catchphrase');
+                      }                
                  ?>
-                <p>La plateforme qui vous permet d’échanger entre voisins des outils et des services de jardinage à bas prix.</p>
+                </h1>
+                
+                <p>
+
+                <?php 
+                    $homepage = new WP_Query([ // je crée une variable $q
+                        'post_type' => 'page', // la je précise quel post_type je veux (dans mon cas "q")    
+                      ]);
+                      while($homepage->have_posts()){
+                        $homepage->the_post();
+                        echo get_field('description-catchphrase');
+                      }                
+                 ?>
+
+                </p>
             </span>
             <?php bouton_hubbert("Je me connecte","index.php","vert nav-item menu-item"); ?>
         </div>
@@ -34,8 +51,32 @@ get_header(); ?>
 
     <section class="outils">
         <span>
-            <h2>Les outils les plus chauds de ta région</h2>
-            <p>Trouves les outils qui te manque chez les personnes qui habitent près de chez toi. Ou alors toi aussi mets à disposition un ou plusieurs de tes outils pour aider et te faire un peu d’économie.</p>
+            <h2>
+
+            <?php 
+                    $homepage = new WP_Query([ // je crée une variable $q
+                        'post_type' => 'page', // la je précise quel post_type je veux (dans mon cas "q")    
+                      ]);
+                      while($homepage->have_posts()){
+                        $homepage->the_post();
+                        echo get_field('sous_titre_middle');
+                      }                
+                 ?>
+
+            </h2>
+            <p>
+
+            <?php 
+                    $homepage = new WP_Query([ // je crée une variable $q
+                        'post_type' => 'page', // la je précise quel post_type je veux (dans mon cas "q")    
+                      ]);
+                      while($homepage->have_posts()){
+                        $homepage->the_post();
+                        echo get_field('sous_titre_middle_description');
+                      }                
+                 ?>
+
+            </p>
         </span>
         <div class="carroussel">
             <?php carroussel_couleur("tools",20); ?>
--- page--configannonce.php
+++ page--configannonce.php
@@ -6,9 +6,17 @@ get_header(); ?>
 
 <?php wpforms(124);?>
 
-<section>
-
-</section>
+<div class="container">
+<?php
+$homepage = new WP_Query([ // je crée une variable $q
+                        'post_type' => 'page', // la je précise quel post_type je veux (dans mon cas "q")    
+                      ]);
+                      while($homepage->have_posts()){
+                        $homepage->the_post();
+                        echo the_content();
+                      } 
+?>
+</div>
 
 
 
