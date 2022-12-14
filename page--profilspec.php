414bdd2af56243ed3342501ac78e0c7e9fdf69a4
Tk1203 <leo.mangez@gmail.com>
2022-12-14 14:17:55 +0100

    profil

--- page--profilspec.php
+++ page--profilspec.php
@@ -2,21 +2,35 @@
 /* Template Name: PageProfilspec */
 get_header(); ?>
 
- 
-<section class="picture">
-        <div class="texte-profil">
-            <span>
-                <h1>Jean Mérique</h1>
-                <p></p>
-            </span>
-        </div>
-    </section>
+<?php
+$content = new WP_Query([ // je crée une variable $services
+  'post_type' => 'profil', // la je précise quel post_type je veux (dans mon cas "services")
+  'post_status' => 'publish', // la je précise que je veux des posts qui sont publié
+  'limit' => 1, // dans mon cas je n'en ai besoin que de trois
+  'orderby' => 'rand', // je les trie par date 
+  'date' => true // je récupéère ma date
+]);
 
-    <?php  $image=get_field('photo_de_profil'); ?>
-	<?php $nom_prenom=get_the_title(); ?>
-	<?php $description=get_the_content(); ?>
+if ($content->have_posts()){ // ici je vérifie que $services posède bien mes posts
 
-
-
-
+      $content->the_post(); // la récupère mon post
+          $image=get_field('photo_de_profil');
+          $nom_prenom=get_the_title();
+          $description=get_the_content();
+          echo '<div class="card profil" style="width: 18rem;">
+              <img class="card-img-top" src="'.$image['url'].'" alt="Card image cap">
+              <div class="card-body">
+                  <h5 class="card-title">'.$nom_prenom.'</h5>
+                  <p class="localite">'.get_field('localite').'</p>
+                  <p class="card-text">'.$description.'</p>
+              </div>
+          </div>
+              ';
+      
+  
+}
+else{
+  echo '<h5>On a pas encore de question a vous répondre mais ça arrive !</h5>';
+}
+?>
 <?php get_footer(); ?>
