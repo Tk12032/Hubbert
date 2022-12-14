edb708cd04b53f9272a720ddb4e433d6e92f8860
LokumBek <lukmanzel007@gmail.com>
2022-12-14 13:01:28 +0100

    lol

--- page--profilspec.php
+++ page--profilspec.php
@@ -2,6 +2,21 @@
 /* Template Name: PageProfilspec */
 get_header(); ?>
 
+ 
+<section class="picture">
+        <div class="texte-profil">
+            <span>
+                <h1>Jean Mérique</h1>
+                <p></p>
+            </span>
+        </div>
+    </section>
+
+    <?php  $image=get_field('photo_de_profil'); ?>
+	<?php $nom_prenom=get_the_title(); ?>
+	<?php $description=get_the_content(); ?>
+
+
 
 
 <?php get_footer(); ?>
