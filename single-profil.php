927bf98acc595d7215e4b2a501faed58e5332f3c
Tk1203 <leo.mangez@gmail.com>
2022-12-16 15:35:15 +0100

    profilspeci

--- single-profil.php
+++ single-profil.php
@@ -2,29 +2,15 @@
 /* Template Name: PageProfilspec */
 get_header(); ?>
 
-<?php
-$content = new WP_Query([ // je crée une variable $services
-  'post_type' => 'profil', // la je précise quel post_type je veux (dans mon cas "services")
-  'post_status' => 'publish', // la je précise que je veux des posts qui sont publié
-  'limit' => 1, // dans mon cas je n'en ai besoin que de trois
-  'orderby' => 'rand', // je les trie par date 
-  'date' => true // je récupéère ma date
-]);
-
-if ($content->have_posts()): // ici je vérifie que $services posède bien mes posts
-
-      $content->the_post(); // la récupère mon post
-          $image=get_field('photo_de_profil');
-          $nom_prenom=get_the_title();
-          $description=get_the_content();
-?>
+<?php $image=get_field('photo_de_profil'); ?>
+ 
 <div class="container">
     <div class="info-profil">
  
         <div class="top-part">
 
             <span class="infotext">
-                <h5><?php echo $nom_prenom; ?></h5>
+                <h5><?php echo the_title(); ?></h5>
                 <h4><?php echo get_field('localite'); ?></h4>
 
             </span>
@@ -32,31 +18,52 @@ if ($content->have_posts()): // ici je vérifie que $services posède bien mes p
 
         </div>
         <span class="descri">                
-            <p><?php echo $description; ?></p>
+            <p><?php echo the_content(); ?></p>
         </span>
 
     </div>
+
     <div id="comments">
-  <?php if ( have_comments() ) : ?>
-    <h3>Commentaires</h3>
-    <ol class="comment-list">
-      <?php wp_list_comments( array( 'style' => 'ol' ) ); ?>
+    <?php if ( have_comments() ) : ?>
+      <h3>Commentaires</h3>
+      <ol class="comment-list">
+        <?php wp_list_comments( array( 'style' => 'ol' ) ); ?>
 
-    </ol>
-  <?php endif; ?>
-  <?php comment_form(); ?>
-</div>
+      </ol>
+      <?php endif; ?>
+      <?php comment_form(); ?>
+  </div>
 
-</div>
+  <div class="commentaires">
 
+  <?php
+// The comment Query
+$comments_query = new WP_Comment_Query();
+$comments       = $comments_query->query( $args );
+?>
+<h3>Commentaires</h3>
 <?php
-else:
-  echo '</h5>'."Il n'y a personne ici !".'</h5>';
-endif;
+// Comment Loop
+if ( $comments ) {
+	foreach ( $comments as $comment ) {
+    echo '<article class="comment">';
+    echo '<div class="vert versprofil">';
+      echo '<div>'.get_avatar($comment).'</div>';
+      echo '<h5>'.$comment->comment_author.'</h5>';
+      echo '</div>';
+      echo '<p>' . $comment->comment_content . '</p>';
+    echo '</article>';
+	}
+} else {
+	echo '<p>'."Cet utilisateur n'a pas encore de commentaires".'</p>';
+}
 ?>
 
+  </div>
 
-<?php comments_template(); ?>
+    
+</div>
+
 
 
 
