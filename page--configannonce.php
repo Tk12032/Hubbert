68cdb67dc2b3a3ef5c56cdc7b7911cfdf4338f7c
Tk1203 <leo.mangez@gmail.com>
2022-12-20 12:04:32 +0100

    du caca

--- page--configannonce.php
+++ page--configannonce.php
@@ -1,8 +1,13 @@
 <?php 
 /* Template Name: PageConfigA */
 get_header(); ?>
-
-
+<div class="container">
+<?php 
+$form = wpcf7_contact_form(219);
+echo $form->form;
+?>
+<?php echo do_shortcode('[frontend_admin field=639708ee1294c edit=false]'); ?>
+</div>
 
 
 
