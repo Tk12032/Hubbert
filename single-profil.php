9dae40cffbe1c9429b78c83f011e848035a7dcd1
LokumBek <lukmanzel007@gmail.com>
2022-12-20 15:08:15 +0100

    profil

--- single-profil.php
+++ single-profil.php
@@ -10,8 +10,11 @@ get_header(); ?>
         <div class="top-part">
 
             <span class="infotext">
-                <h5><?php echo the_title(); ?></h5>
-                <h4><?php echo get_field('localite'); ?></h4>
+                <h5><?php echo the_title(); ?></h4>
+                <h5><?php echo get_field('age'); ?></h5>
+                <h5><?php echo get_field('localite'); ?></h5>
+                <h5><?php echo get_field('numero'); ?></h5>
+               
 
             </span>
             <img class="pdp" src="<?php echo $image['url']; ?>" alt="Photo d'un très bel homme">
@@ -48,13 +51,11 @@ if ( comments_open() || get_comments_number() ) :
   comment_form(
     array(
         'title_reply'       => __( 'Commente quelque chose' ),
-        'label_submit'      => __( 'Poster' ),
-        'logged_in_as' => '',
-        'comment_field'     => '<p><textarea id="comment" name="comment" aria-required="true"></textarea></p>',
+        'label_submit'      => __( 'Post Comment' ),
+        'comment_field'     => '<p class="comment-form-comment"><label for="comment">' . _x( 'Comment', 'noun' ) . '</label><br /><textarea id="comment" name="comment" aria-required="true"></textarea></p>',
     )
 );
 endif;
-
 ?>
 <h3>Commentaires</h3>
 <?php
@@ -65,7 +66,7 @@ if ( $comments ) {
 	foreach ( $comments as $comment ) {
     $comment_post_id = $comment->comment_post_ID;
     if($comment_post_id==$current_post_id){
-      echo '<article class="comment">';
+      echo '<article class="comm  ent">';
       echo '<div class="vert versprofil">';
         echo '<div>'.get_avatar($comment).'</div>';
         echo '<h5>'.$comment->comment_author.'</h5>';
