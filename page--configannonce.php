c56360201edd8eabc12661608a3ff64cd32f4718
Tk12032 <leo.mangez@gmail.com>
2022-12-27 12:55:34 +0100

    yo

--- page--configannonce.php
+++ page--configannonce.php
@@ -2,14 +2,10 @@
 /* Template Name: PageConfigA */
 get_header(); ?>
 <div class="container">
-<?php 
-$form = wpcf7_contact_form(219);
-echo $form->form;
-?>
-<?php echo do_shortcode('[frontend_admin field=639708ee1294c edit=false]'); ?>
+
+
+
+
 </div>
-
-
-
 
 <?php get_footer(); ?>
