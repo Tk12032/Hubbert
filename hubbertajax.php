0b62bcfb41e0c1739d12b3eafe4a225e89e3d7f4
Tk12032 <leo.mangez@gmail.com>
2023-01-06 18:00:49 +0100

    le fiiiiiiltre

--- hubbertajax.php
+++ hubbertajax.php
@@ -16,8 +16,85 @@ function create_mypost(){
 }
 add_action('wp_ajax_create_post', 'create_mypost');
 
+function filter(){
+	
+	if($_GET['outil']=='true'){
+		$outil='outil';
+	}else{$outil='';}
+	if($_GET['service']=='true'){
+		$service='service';
+	}else{$service='';}
+	$annoncefiltrées = new WP_Query([
+		
+		
+		'post_type' => 'annonce', 
+		'post_status' => 'publish', 
+		'limit' => 10,
+		'orderby' => 'date',
+		'date' => true,
+		'meta_query'    => array(
+			'relation'      => 'AND',
+			array(
+				'key'       => 'prix',
+				'value'     => $_GET['prixmax'],
+				'type'      => 'NUMERIC',
+				'compare'   => '<'
+			),
+			array(
+					'key'       => 'type_dannonce',
+					'value'     =>	array($outil, $service),
+					'compare'   => 'IN'
+			),
+		),
+	  ]); 
+
+	if($annoncefiltrées->have_posts()){
+		$i=0;
+		$tableaureponse;
+		while($annoncefiltrées->have_posts()){
+			$annoncefiltrées->the_post();
+
+
+			$image = get_field('image_dillustration');
+			$a_id=$post->post_author;
+			$auteur = get_the_author_meta( 'user_nicename', $a_id );
+			if($auteur===''){
+				$authorname = 'Anonyme';
+				}
+				else{
+					$authorname = $auteur;
+			}
+
+			$tableaureponse[$i] = array(
+				"type" => get_field('type_dannonce'),
+				"urlimage" => $image['url'],
+				"title" => get_the_title(),
+				"content" => get_field('description'),
+				"urlavatar" => get_avatar_url($auteur),
+				"authorname" => $authorname,
+				"prix" => get_field('prix'),
+				"lienarticle" => get_the_permalink(),
+			  );
+			  $i++;
+			}
+			
+			$json = json_encode($tableaureponse);
+
+			echo $json;
+
+	}
+	else echo 'none';
+	
+	
+	
+	die();
+}
+
+
+add_action('wp_ajax_filter','filter');
 
 
 
 do_action('wp_ajax_'.$_POST['action']);
+do_action('wp_ajax_'.$_GET['action']);
 ?>
--- page--recherche.php
+++ page--recherche.php
@@ -7,12 +7,12 @@ get_header(); ?>
     <aside class="filtres" style="width:270px">
         <div class ="filtre-check-box">
             <div class="check">  
-            <input class="checkbox-effect" id="outils" type="checkbox" name="outils"/>
-                <label for="outils">Outils</label>
+            <input class="checkbox-effect" id="checkoutils" type="checkbox" name="checkoutils"/>
+                <label for="checkoutils">Outils</label>
             </div>
             <div class="check">
-                <input class="checkbox-effect" id="services" type="checkbox" name="services"/>
-                <label for="services">Services</label>
+                <input class="checkbox-effect" id="checkservices" type="checkbox" name="checkservices"/>
+                <label for="checkservices">Services</label>
             </div>
         </div>
 
@@ -20,13 +20,13 @@ get_header(); ?>
 
         <div style="display:flex; flex-direction:column">
             <label for="prix">Prix</label>
-            <input type="range" name="prix" value="15" min="1" max="100" oninput="this.nextElementSibling.value = this.value + ' €/j'">
+            <input id="prix" type="range" name="prix" value="15" min="1" max="100" oninput="this.nextElementSibling.value = this.value + ' €/j'">
             <output style="margin:10px 0px">15 €/j</output>
         </div>
 
         <div style="display:flex; flex-direction:column">
             <label for="distance">Distance</label>
-            <input type="range" name="distance" value="10" min="1" max="100" oninput="this.nextElementSibling.value = this.value + ' km'">
+            <input id="distance" type="range" name="distance" value="10" min="1" max="100" oninput="this.nextElementSibling.value = this.value + ' km'">
             <output style="margin:10px 0px">10 km</output>
         </div>
 
@@ -51,66 +51,17 @@ get_header(); ?>
             <div style="display:flex;justify-content:space-between;margin-top:10px"><output id="starthour">7h</output><output id="endhour">20h</output></div>
         </div>
         
-        <button class="vert" style="display:flex; width:fit-content;padding:10px;align-items:center">
+        <button class="vert" style="display:flex; width:fit-content;padding:10px;align-items:center" onclick="filter()">
             <img style="width:20px" class="recherchepicto" src="<?php echo get_template_directory_uri() ?>/assets/img/picto_recherche_inact.svg">
             <p style="margin:0px 10px;">C'est parti !</p>
         </button>
     </aside>
 
-    <section style="width:100%" class="recherche">
+    <section style="width:100%" id="parent" class="recherche">
 
-        <?php  $content = new WP_Query([ // je crée une variable $services
-        'post_type' => 'annonce', // la je précise quel post_type je veux (dans mon cas "services")
-        'post_status' => 'publish', // la je précise que je veux des posts qui sont publié
-        'limit' => 10, // dans mon cas je n'en ai besoin que de trois
-        'orderby' => 'date', // je les trie par date 
-        'date' => true // je récupéère ma date
-        ]);
+        
 
-        if($content->have_posts()):?>
-            <?php while($content->have_posts()):
-                $content->the_post();?>
-                <article class="<?php echo get_field('type_dannonce').' '.get_field('type_doutil').' '.get_field('type_de_service').' '.get_field('prix').' '.get_field('localite').' '.get_field('autre_outil').' '.get_field('autre_service')?>">
-
-                    <div class="card flex-row">
-                            <img class="card-img-left example-card-img-responsive" style="min-width:460px; height:330px" src="<?php $image=get_field('image_dillustration'); echo $image['url']?>">
-                            <div class="card-body">
-                                <div>
-                                    <h4 class="card-title h5 h4-sm" style="font-family:'Poppins';text-transform: uppercase;font-weight:600;font-size:24px"><?php echo get_the_title()?></h4>
-                                    <p class="card-text"><?php echo get_field('description')?></p>
-                                </div>
-                                
-
-                                <div class="bot-card-body">
-
-                                    <?php
-                                    $a_id=$post->post_author;
-                                    $auteur = get_the_author_meta( 'user_nicename', $a_id );
-                                    echo '<div class="vert versprofil">';
-                                    echo get_avatar($auteur);?>
-                                    <h5>
-                                    <?php if($auteur===''){
-                                    echo 'Anonyme';
-                                    }
-                                    else{
-                                            echo $auteur;
-                                    }
-                                    echo '</h5> </div>'; 
-                                    ?>
-
-                                    <h5><?php echo get_field('prix') ?>€ par jour</h5>
-
-                                    <?php echo '<a href="'.get_the_permalink().'" class="vert boutonplus">+</a>';
-                                    ?>
-                            </div>
-                    </div>
-                </article>
-            <?php endwhile;?>
-        <?php else:
-        echo "Il n'y a pas encore d'annonce sur ce site... Bizarre...";
-        endif;?>
-
-
+        <div id="readmore"></div>
     </section>
 
 </div>
