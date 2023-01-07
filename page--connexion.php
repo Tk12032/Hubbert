e435f483038f2c79653f9de82a89709562069097
LokumBek <lukmanzel007@gmail.com>
2023-01-07 18:07:21 +0100

    page connexion (cassé ? jsp)

--- page--connexion.php
+++ page--connexion.php
@@ -2,6 +2,38 @@
 /* Template Name: PageConnexion */
 get_header(); ?>
 
+<h1 class='texte_connect h1'> Connecte toi à Hubbert </h1>
+<br>
+<br>
+<p class='texte_connect p'> Complete le plus possible ton profil pour ne pas devoir y retourner plus tard </p>
+<br>
+<br>
+<div class="container_connexion"> </div>
 
+<p class='connect'> Nom </p>
+
+<section class="rectangle_connect"> </section>
+
+<p class='connect'> Prénom </p>
+
+<section class="rectangle_connect"> </section>
+
+<p class='connect'> localité </p>
+
+<section class="rectangle_connect"> </section>
+
+<p class='connect'> Mail </p>
+
+<section class="rectangle_connect"> </section>
+
+<p class='connect'> Mot de passe </p>
+
+<section class="rectangle_connect"> </section>
+
+<p class='connect'> Confirmer le mot de passe </p>
+
+<section class="rectangle_connect"> </section>
+
+<section class="orange_connect"> Je m'inscris</section>
 
 <?php get_footer(); ?>
